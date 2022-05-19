a45b58e2ddd0275912197d3069dbccd743fa1f56
maxlestage <maxlestage@icloud.com>
2022-05-19 16:53:28 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
+const mongoUrl = "mongodb://localhost/bloglist";
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const app = require("./app");
+const cors = require("cors");
+
+const server = http.createServer(app);
+
+const PORT = 3003;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
